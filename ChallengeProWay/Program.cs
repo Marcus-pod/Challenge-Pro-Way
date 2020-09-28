98aad7bf965a6c0687fcdf279edb1c69b5d70c9b
Marcus Vinícius <marcussvgb@hotmail.com>
2020-09-28 16:00:26 -0300

    Iniciando o projeto e declaração das variáveis

---
+++ ChallengeProWay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChallengeProWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe o placar do jogo:");
+            int scoreboard = int.Parse(Console.ReadLine());
+
+            string newGame = "";
+            int jogo = 1;
+            int minSeason = scoreboard;
+            int maxSeason = scoreboard;
+            int minRecord = 0;
+            int maxRecord = 0;
+
+
+            Console.ReadLine();
+        }
+    }
+}
