ddb503c6bbfeeb56340af4ce7d3f645d34c0ddbc
Marcus Vinícius <marcussvgb@hotmail.com>
2020-09-28 16:10:29 -0300

    Estrutura de repetição

--- ChallengeProWay/Program.cs
+++ ChallengeProWay/Program.cs
@@ -17,6 +17,23 @@ namespace ChallengeProWay
             int minRecord = 0;
             int maxRecord = 0;
 
+            while (newGame != "Nao")
+            {
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("|   Jogo    |   Placar    |   Mínimo da Temporada   |   Máximo da Temporada   |   Quebra recorde min.   |   Quebra recorde máx.   |");
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("|     " + jogo + "     |     " + scoreboard + "      |           " + minSeason + "            |           "
+                    + maxSeason + "            |             " + minRecord + "           |             " + maxRecord + "           |");
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------");
+
+                // cadastrando um novo jogo
+                Console.WriteLine();
+                Console.WriteLine("Deseja adicionar um novo jogo? (Sim/Nao)");
+                newGame = Console.ReadLine();
+                Console.WriteLine();
+
+            }
+
 
             Console.ReadLine();
         }
